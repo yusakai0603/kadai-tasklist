f04b6e4d0b45374f45800f864c21fda296f82dcf
yu-sakai <yu-sakai@NA21034.nature.local>
2021-07-27 11:06:33 +0900

    データベース関連のファイルを追加2

--- src/main/java/controllers/IndexServlet.java
+++ src/main/java/controllers/IndexServlet.java
@@ -1,33 +1,35 @@
+//データベースから複数のメッセージ情報を取得して一覧表示するサーブレット
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class IndexServlet
- */
+import models.Tasks;
+import utils.DBUtil;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+
     public IndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Tasks> messages = em.createNamedQuery("getAllMessages", Tasks.class).getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
+    }
 
 }
--- src/main/java/models/Tasks.java
+++ src/main/java/models/Tasks.java
@@ -10,15 +10,15 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "messages")
-public class Message {
+@Table(name = "tasks")
+public class Tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
+//    @Column(name = "title", length = 255, nullable = false)
+//    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -37,17 +37,17 @@ public class Message {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
+//    public String getTitle() {
+//        return title;
+//    }
+//
 //    public void setTitle(String title) {
 //        this.title = title;
 //    }
-//
-//    public String getContent() {
-//        return content;
-//    }
+
+    public String getContent() {
+        return content;
+    }
 
     public void setContent(String content) {
         this.content = content;
